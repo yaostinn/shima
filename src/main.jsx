a9ef2f556c0ece960a1ca5c5bb684e9b605c2d86
yaostinn <yaostinn@gmail.com>
2023-12-20 23:56:21 +0300

    add: dev branch

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+//add dev branch
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider>
