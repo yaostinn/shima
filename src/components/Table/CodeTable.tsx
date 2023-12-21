85a4f17356320544c720506ea43098f119ae255c
Elvinsky <mikhnevichnik@gmail.com>
2023-12-21 10:38:46 +0100

    refactor: refactored styles

--- src/components/Table/CodeTable.tsx
+++ src/components/Table/CodeTable.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Table } from "@mantine/core";
 import Code from "../Code/Code";
 import { CodeTableData } from "../../types";
+import "./table.css";
 
 const CodeTable: React.FC = () => {
   const [data, setData] = useState<CodeTableData[]>([]);
@@ -29,38 +30,45 @@ const CodeTable: React.FC = () => {
   };
 
   return (
-    <>
+    <div className="wrapper">
       <Code onFormSubmit={handleFormSubmit} />
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>ID</Table.Th>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>Code</Table.Th>
-            <Table.Th>Execution Response</Table.Th>
-            <Table.Th>Created at</Table.Th>
-            <Table.Th>Updated at</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-
-        <Table.Tbody>
-          {data.map((el) => (
-            <Table.Tr key={el.id}>
-              <Table.Td>{el.id}</Table.Td>
-              <Table.Td>
-                <a key={el.id} href={`/code/${el.id}`}>
-                  {el.name}
-                </a>
-              </Table.Td>
-              <Table.Td>{el.code}</Table.Td>
-              <Table.Td bg="green">{el.execution_response}</Table.Td>
-              <Table.Td>{el.created_at}</Table.Td>
-              <Table.Td>{el.updated_at}</Table.Td>
+      <Table className="table">
+        <div className="table-wrapper">
+          <Table.Thead>
+            <Table.Tr className="table__tr">
+              <Table.Th className="table__th">ID</Table.Th>
+              <Table.Th className="table__th">Name</Table.Th>
+              <Table.Th className="table__th">Code</Table.Th>
+              <Table.Th className="table__th">Execution Response</Table.Th>
+              <Table.Th className="table__th">Created at</Table.Th>
+              <Table.Th className="table__th">Updated at</Table.Th>
             </Table.Tr>
-          ))}
-        </Table.Tbody>
+          </Table.Thead>
+          <Table.Tbody>
+            {data.map((el) => (
+              <Table.Tr key={el.id} className="tohover">
+                <Table.Td className="table__td">{el.id}</Table.Td>
+                <Table.Td className="table__td">
+                  <a key={el.id} href={`/code/${el.id}`}>
+                    {el.name}
+                  </a>
+                </Table.Td>
+                <Table.Td className="table__td">{el.code}</Table.Td>
+                <Table.Td bg="green" className="table__td">
+                  {el.execution_response}
+                </Table.Td>
+                <Table.Td className="table__td">
+                  {new Date(el.created_at).toDateString()}
+                </Table.Td>
+                <Table.Td className="table__td">
+                  {new Date(el.updated_at).toDateString()}
+                </Table.Td>
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </div>
       </Table>
-    </>
+    </div>
   );
 };
 
