1d5abb16d6252d3ff87b6653a207a26baebead91
yaostinn <yaostinn@gmail.com>
2023-12-23 02:04:40 +0300

    change: placeholder, add: readme

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
-# React + Vite
+# SavaScript
+## stack
+#### UI-backend:
+![Django](https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white) ![DjangoREST](https://img.shields.io/badge/DJANGO-REST-ff1709?style=for-the-badge&logo=django&logoColor=white&color=ff1709&labelColor=gray) ![Postgres](https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white) ![Gunicorn](https://img.shields.io/badge/gunicorn-%298729.svg?style=for-the-badge&logo=gunicorn&logoColor=white) ![Nginx](https://img.shields.io/badge/nginx-%23009639.svg?style=for-the-badge&logo=nginx&logoColor=white)
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+#### SavaScrip-backend:
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB) ![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white)
 
-Currently, two official plugins are available:
+#### UI-frotend
+![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![]()
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+#### Testing
+![Jest](https://img.shields.io/badge/-jest-%23C21325?style=for-the-badge&logo=jest&logoColor=white) ![cypress](https://img.shields.io/badge/-cypress-%23E5E5E5?style=for-the-badge&logo=cypress&logoColor=058a5e) ![Selenium](https://img.shields.io/badge/-selenium-%43B02A?style=for-the-badge&logo=selenium&logoColor=white)
+
+
+
+
+
+
+
+![Alt text](image.png)
--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -1,51 +1,51 @@
-import React from "react";
-import "./Code.css";
+import React from 'react'
+import './Code.css'
 
-import { TextInput, Paper, Textarea, Button } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { CodeProps } from "../../types";
+import { TextInput, Paper, Textarea, Button } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import { CodeProps } from '../../types'
 
 const Code: React.FC<CodeProps> = ({ onFormSubmit }) => {
   const form = useForm({
     initialValues: {
-      name: "",
-      code: "",
+      name: '',
+      code: '',
     },
-  });
-  const handleResponseSava=async()=>{
-    const response = await fetch('http://localhost:8000/submit',{
-      headers:{
-        "Content-Type":"application/json"
+  })
+  const handleResponseSava = async () => {
+    const response = await fetch('http://localhost:8000/submit', {
+      headers: {
+        'Content-Type': 'application/json',
       },
-      method: "POST",
-      body: JSON.stringify(form.values.code)
+      method: 'POST',
+      body: JSON.stringify(form.values.code),
     })
 
-    const data = await response.json();
+    const data = await response.json()
 
-    return data.stdout;
+    return data.stdout
   }
 
   const handleSubmit = async () => {
-    const formData = new FormData();
+    const formData = new FormData()
 
-    formData.append("name", form.values.name);
-    formData.append("code", form.values.code);
-    formData.append("execution_response", await handleResponseSava());
+    formData.append('name', form.values.name)
+    formData.append('code', form.values.code)
+    formData.append('execution_response', await handleResponseSava())
 
     const response = await fetch(
-      "https://nikostin.pythonanywhere.com/api/codes/",
+      'https://nikostin.pythonanywhere.com/api/codes/',
       {
-        method: "POST",
+        method: 'POST',
         body: formData,
       }
-    );
+    )
 
     if (response.ok) {
-      form.reset();
-      onFormSubmit(); // Refresh data after successful form submission
+      form.reset()
+      onFormSubmit() // Refresh data after successful form submission
     }
-  };
+  }
 
   return (
     <Paper className="code" data-testid="cypress-form">
@@ -53,28 +53,30 @@ const Code: React.FC<CodeProps> = ({ onFormSubmit }) => {
         id="name"
         label="Имя"
         description="Оно сохранится на веб"
-        placeholder="Input placeholder"
-        {...form.getInputProps("name")}
+        placeholder="ex: Никита Остин"
+        {...form.getInputProps('name')}
         className="text"
         data-testid="cypress-form-name"
       />
       <Textarea
         id="code"
         label="Code"
-        description="Save"
-        placeholder="Input placeholder"
-        {...form.getInputProps("code")}
+        placeholder="SavaScript, ex: (2+2)*3"
+        {...form.getInputProps('code')}
         className="text"
         data-testid="cypress-form-code"
       />
 
-
-      <Button onClick={handleSubmit} fullWidth mt="xs" data-testid="cypress-form-button">
-
+      <Button
+        onClick={handleSubmit}
+        fullWidth
+        mt="xs"
+        data-testid="cypress-form-button"
+      >
         Сохранить
       </Button>
     </Paper>
-  );
-};
+  )
+}
 
-export default Code;
+export default Code
